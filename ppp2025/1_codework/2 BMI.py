f339059801e473ada3b3c21c87a2c6a4e4f5a218
HSJ02 <117615107+HSJ02@users.noreply.github.com>
2025-03-19 14:13:21 +0900

    Delete ppp2025 directory

--- ppp2025/1_codework/2 BMI.py
+++
@@ -1,8 +0,0 @@
-weight = int(input("몸무게(kg)는 얼마에요? => "))
-height = int(input("키(cm)가 얼마인가요? => "))
-
-height_m = height / 100
-
-BMI = weight / height_m ** 2  
-
-print("키는 {}cm, 몸무게는 {}kg일 때, BMI는 {}입니다.".format(height, weight, BMI))
--- ppp2025/1_codework/4 Trapezoid.py
+++
@@ -1,7 +0,0 @@
-upper = int(input("윗변의 길이를 알려주세요 => "))
-lower = int(input("밑변의 길이를 알려주세요 => "))
-height = int(input("마지막으로 높이를 알려주세요 => "))
-area = (upper + lower)*height/2
-
-print("윗변={}, 밑변={}, 높이={}일 때,".format(upper, lower, height))
-print("사다리꼴의 넓이 => {}".format(area))
