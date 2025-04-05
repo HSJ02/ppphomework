a7e362e6df90bd20b1bc9ff96a713cf3a747436e
황승재 <117615107+HSJ02@users.noreply.github.com>
2025-04-05 21:40:14 +0900

    Add files via upload

--- 07codework/hw05split.py
+++ 07codework/hw05split.py
@@ -1,14 +1,14 @@
 from hw01average import average
 
 def main():
-    text = input("정수 값을 나열해주세요 (예시: 10 20 30) : ")
+    text = input("정수를 순서대로 기입하세요. 예시(10 20 30) : ")
     text_list = text.split()
 
-    n = []
+    nums = []
     for x in text_list:
-        n.append(int(x))
+        nums.append(int(x))
 
-    print(average(n))
+    print(f"평균은 {average(nums):.1f}입니다.")
 
 if __name__ == "__main__":
     main()
