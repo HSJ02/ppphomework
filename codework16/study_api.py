f185fe56c2e53de917770a43bd82c3adf6282d0d
SeungJae Hwang <117615107+HSJ02@users.noreply.github.com>
2025-05-24 20:53:35 +0900

    Update study_api.py

--- codework16/study_api.py
+++ codework16/study_api.py
@@ -61,25 +61,40 @@ def download_weather(station_id, year, filename):
         resp.encoding = "UTF-8"
         f.write(resp.text)
 
+def download_weather2(station_id, year, filename):
+    URL= f"https://api.taegon.kr/stations/146/?sy=2001&ey=2022&format=csv"
+    with open(filename, "w", encoding="UTF-8-sig") as f:
+        resp = requests.get(URL)
+        resp.encoding = "UTF-8"
+        f.write(resp.text)
+
+def download_weather3(station_id, year, filename):
+    URL= f"https://api.taegon.kr/stations/146/?sy=2024&ey=2024&format=csv"
+    with open(filename, "w", encoding="UTF-8-sig") as f:
+        resp = requests.get(URL)
+        resp.encoding = "UTF-8"
+        f.write(resp.text)
+
 def main():
     filename = "codework16/weather(119)_2024-2024.csv"
     if not os.path.exists(filename):
-        download_weather(119, 2020, "codework16/weather(119)_2024-2024.csv")
+        download_weather(119, 2024, "codework16/weather(119)_2024-2024.csv")
     file_jeonju = "codework16/weather(146)_2001-2022.csv"
+    if not os.path.exists(file_jeonju):
+        download_weather2(146, 2001-2022, "codework16/weather(146)_2001-2022.csv")
     file_jeonju2024 = "codework16/weather(146)_2024-2024.csv"
+    if not os.path.exists(file_jeonju2024):
+        download_weather3(146, 2024, "codework16/weather(146)_2024-2024.csv")
 
     rainfall = get_weather_db(file_jeonju, 9)
     year = get_weather_db2(file_jeonju, 0)
-    print(f"{sumifs(rainfall, year, selected=[2015]):.0f}")
     problem01 = f"{sumifs(rainfall, year, selected=[2015]):.0f}"
 
     tmax = get_weather_db(file_jeonju, 3)
     tmax2 = get_weather_db(file_jeonju2024, 3)
 
     tmin = get_weather_db(file_jeonju2024, 5)
-    print(f"{tmaxifs(tmax, year, selected=[2022]):.0f}")
     problem02 = f"{tmaxifs(tmax, year, selected=[2022]):.0f}"
-    print(f"{max_gap(tmax2, tmin):.0f}")
     problem03 = f"{max_gap(tmax2, tmin):.0f}"
 
     suwon_rain = get_weather_db(filename, 9)
@@ -97,8 +112,12 @@ def main():
     answer2 = problem02
     answer3 = problem03
     answer4 = problem04
-
+    print(f"제출코드 : {problem01}, {problem02}, {problem03}, {problem04}.")
     submit_to_api(name, affiliation, student_id, answer1, answer2, answer3, answer4, verbose=True)
+
+if __name__ == "__main__":
+    main()
+
 
 if __name__ == "__main__":
     main()
